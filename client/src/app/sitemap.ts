2e73dd59f4346542afbddd7faddff283b35fa0d4
romankarbivskyi <romankarbivskyi1@gmail.com>
2025-03-09 16:31:31 +0200

    update: sitemap urls

--- client/src/app/sitemap.ts
+++ client/src/app/sitemap.ts
@@ -8,5 +8,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 1,
     },
+    {
+      url: "https://mmasters.live/privacy-policy",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://mmasters.live/terms",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
