fd66560208d8f7f41fffc898bacce1ba3ca0653b
romankarbivskyi <romankarbivskyi1@gmail.com>
2025-03-10 18:41:23 +0200

    fix: google adsense link

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -92,7 +92,6 @@ export default function RootLayout({
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1430857171678326"
-          crossorigin="anonymous"
         ></script>
       </head>
       <body
